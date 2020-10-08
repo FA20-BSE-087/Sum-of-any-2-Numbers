09e28097bec559258ee5387bf874f5d285664f78
Zohaib Ahmad <72567960+FA20-BSE-087@users.noreply.github.com>
2020-10-08 21:50:01 +0500

    Add files via upload

---
+++ sum of 2 number calculator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    scanf("%i" ,&a);
+
+    int b;
+ scanf("%i",&b);
+
+ int c=a+b;
+    printf("Sum is : ");
+   printf("%i",c);
+    return 0;
+}
